202dd6c0957359931e0669ec387cc109b9058b88
elm <elm@skweez.net>
2013-09-29 23:38:42 +0200

    Remove test discussion since creating new discussions from the webapp works

--- src/main/java/net/skweez/forum/datastore/simple/SimpleDatastoreFactory.java
+++ src/main/java/net/skweez/forum/datastore/simple/SimpleDatastoreFactory.java
@@ -2,7 +2,6 @@ package net.skweez.forum.datastore.simple;
 
 import net.skweez.forum.datastore.DatastoreFactory;
 import net.skweez.forum.datastore.DiscussionDatastore;
-import net.skweez.forum.model.Discussion;
 
 /**
  * Factory that creates simple datastore objects with in-memory storage. This
@@ -18,11 +17,6 @@ public class SimpleDatastoreFactory extends DatastoreFactory {
 	public DiscussionDatastore getDiscussionDatastore() {
 		if (datastore == null) {
 			datastore = new SimpleDiscussionDatastore();
-
-			Discussion discussion = new Discussion();
-			discussion.setId(-1);
-			discussion.setTitle("Test discussion");
-			datastore.createDiscussion(discussion);
 		}
 		return datastore;
 	}
