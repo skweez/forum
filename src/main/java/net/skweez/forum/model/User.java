098f67b3772e73873e4888f590479b3521797e6a
elm <elm@skweez.net>
2013-10-01 17:57:42 +0200

    Initial user class

--- src/main/java/net/skweez/forum/model/User.java
+++ src/main/java/net/skweez/forum/model/User.java
@@ -1,5 +1,50 @@
 package net.skweez.forum.model;
 
+/**
+ * User class.
+ * 
+ * @author elm
+ * 
+ */
 public class User {
+	/**
+	 * the id
+	 */
+	private int id;
+
+	/**
+	 * the name
+	 */
+	private String name;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
