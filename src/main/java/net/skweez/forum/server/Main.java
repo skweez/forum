f6ad181ed086a86fca64e688ebc32e2a142ba95e
elm <elm@skweez.net>
2013-10-04 16:02:59 +0200

    Add test user to embedded jetty

--- src/main/java/net/skweez/forum/server/Main.java
+++ src/main/java/net/skweez/forum/server/Main.java
@@ -3,7 +3,10 @@
  */
 package net.skweez.forum.server;
 
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.HashLoginService;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.security.Credential;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
@@ -23,6 +26,18 @@ public class Main {
 		context.setResourceBase("src/main/webapp");
 		context.setContextPath("/");
 		context.setParentLoaderPriority(true);
+
+		HashLoginService loginService = new HashLoginService();
+		loginService.putUser("testUser1",
+				Credential.getCredential("testPassword1"),
+				new String[] { "user" }); // Role of this user
+		loginService.setName("net.skweez.forum");
+
+		ConstraintSecurityHandler securityHandler = new ConstraintSecurityHandler();
+		securityHandler.setLoginService(loginService);
+		securityHandler.setRealmName("net.skweez.forum");
+
+		context.setSecurityHandler(securityHandler);
 
 		server.setHandler(context);
 
