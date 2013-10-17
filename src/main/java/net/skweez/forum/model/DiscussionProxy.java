d500d74fd83618086168d3f2709f30483b36d002
elm <elm@skweez.net>
2013-10-17 18:01:40 +0200

    Update comments

--- src/main/java/net/skweez/forum/model/DiscussionProxy.java
+++ src/main/java/net/skweez/forum/model/DiscussionProxy.java
@@ -7,41 +7,69 @@ import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
 /**
- * Adaptor/Proxy for Discussions. Provides all the members of discussions that
- * are needed by XStream to serialize and deserialize Discussions.
+ * Proxy for Discussions. Provides all the members of discussions that are
+ * needed by XStream to serialize and deserialize Discussions.
  * 
  * @author elm
  * 
  */
 @XStreamAlias("Discussion")
 public class DiscussionProxy {
+	/** the actual discussion */
 	@XStreamOmitField
 	private Discussion discussion;
 
 	/**
-	 * 
+	 * Creates a new DiscussionProxy for a new discussion.
 	 */
 	public DiscussionProxy() {
 		discussion = new Discussion();
 	}
 
+	/**
+	 * Creates a new DiscussionProxy for a given discussion.
+	 * 
+	 * @param discussion
+	 *            the discussion
+	 */
 	public DiscussionProxy(Discussion discussion) {
 		this.discussion = discussion;
 	}
 
+	/**
+	 * @return the actual discussion
+	 */
 	public Discussion getDiscussion() {
 		return discussion;
 	}
 
+	/**
+	 * Sets a post for the discussion. This is used by XStream when
+	 * deserializing a DiscussionProxy object.
+	 * 
+	 * @param post
+	 */
 	public void setPost(Post post) {
+		assert (discussion.getPosts().size() == 0);
 		discussion.addPost(post);
 	}
 
+	/**
+	 * Sets the discussion title.
+	 * 
+	 * @param title
+	 *            the title
+	 */
 	public void setTitle(String title) {
 		discussion.setTitle(title);
 	}
 
-	public String getTitle(String title) {
+	/**
+	 * Returns the discussions title
+	 * 
+	 * @return the title
+	 */
+	public String getTitle() {
 		return discussion.getTitle();
 	}
 }
--- src/main/java/net/skweez/forum/model/PostProxy.java
+++ src/main/java/net/skweez/forum/model/PostProxy.java
@@ -8,27 +8,43 @@ import java.util.Date;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
 
 /**
+ * Proxy for Posts. Provides all the members of post that are needed by XStream
+ * to serialize and deserialize a post.
+ * 
  * @author elm
  * 
  */
 public class PostProxy {
+	/** the actual post */
 	@XStreamOmitField
 	private Post post;
 
+	/**
+	 * @return the content
+	 */
 	public String getContent() {
 		return post.getContent();
 	}
 
+	/**
+	 * Set the content of the post.
+	 * 
+	 * @param content
+	 *            the content of the post
+	 */
 	public void setContent(String content) {
 		post.setContent(content);
 	}
 
+	/**
+	 * @return the date of the post
+	 */
 	public Date getDate() {
 		return post.getDate();
 	}
 
 	/**
-	 * 
+	 * Creates a new PostProxy for a new Post object.
 	 */
 	public PostProxy() {
 		post = new Post();
