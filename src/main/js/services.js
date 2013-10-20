8963333c900f45a807496670b7e84e2d5f2ab249
elm <elm@skweez.net>
2013-10-20 20:28:42 +0200

    Fix typos

--- src/main/js/services.js
+++ src/main/js/services.js
@@ -1,13 +1,14 @@
 goog.provide('net.skweez.forum.services');
 
 goog.require('net.skweez.forum.app');
-goog.require('net.skweez.forum.DiscussionService');
+goog.require('net.skweez.forum.DiscussionsService');
+goog.require('net.skweez.forum.PostsService');
 goog.require('net.skweez.forum.AlertService');
 goog.require('net.skweez.forum.UserService');
 
 var services = angular.module('net.skweez.forum.services', [ 'ngResource' ]);
 
 services.factory('Discussions', DiscussionsService());
-services.factory('Posts', PostService());
+services.factory('Posts', PostsService());
 services.service('AlertService', AlertService());
 services.factory('UserService', UserService());
