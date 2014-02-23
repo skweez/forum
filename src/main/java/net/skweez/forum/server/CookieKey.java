167d0f582b8a2321366e196917218c6e61302e9a
elm <elm@skweez.net>
2014-02-23 12:31:19 +0100

    Fix review comments
    * Extract constants for cookie keys
    * Catch NumberFormatException

---
+++ src/main/java/net/skweez/forum/server/CookieKey.java
@@ -0,0 +1,13 @@
+package net.skweez.forum.server;
+
+/** The keys that are used in cookies on the client. */
+/* package */class CookieKey {
+	/** The name of the session id cookie. */
+	public static final String UID = "uid";
+
+	/** The name of the auth token cookie. */
+	public static final String SESSION_ID = "sessionId";
+
+	/** The name of the UID cookie. */
+	public static final String AUTH_TOKEN = "authToken";
+}
