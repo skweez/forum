df479f80bb9f7aa4c2739e3ced73ad5884b0e7d8
Michael Kanis <mks@skweez.net>
2014-02-24 16:48:52 +0100

    Add interface docs

--- src/main/java/net/skweez/forum/utils/JsonUtils.java
+++ src/main/java/net/skweez/forum/utils/JsonUtils.java
@@ -5,13 +5,24 @@ import java.io.InputStreamReader;
 
 import com.google.gson.Gson;
 
+/**
+ * Utility class for serialization and deserialization of objects to/from JSON.
+ * 
+ * @author mks
+ */
 public class JsonUtils {
 
+	/** Serialize the given object. */
 	public static String serialize(Object object) {
 		return new Gson().toJson(object);
 	}
 
-	@SuppressWarnings("unchecked")
+	/**
+	 * Deserialize an object from the stream.
+	 * 
+	 * @throws Exception
+	 *             if the object cannot be deserialized.
+	 */
 	public static <T> T deserialize(InputStream inputStream,
 			Class<T> expectedClass) throws Exception {
 		return new Gson().fromJson(new InputStreamReader(inputStream),
